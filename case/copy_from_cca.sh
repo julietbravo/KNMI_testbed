fb969ca7767c995c57f369ff307ee385039c04e9
julietbravo <bart@vanstratum.com>
2018-07-20 14:30:10 +0000

    Update DALES case

---
+++ case/copy_from_cca.sh
@@ -0,0 +1,2 @@
+scp -r cca:/scratch/ms/nl/nkbs/DALES/KNMI_testbed/namoptions.001 .
+scp -r cca:/scratch/ms/nl/nkbs/DALES/KNMI_testbed/run.PBS .
---
+++ case/copy_to_cca.sh
@@ -0,0 +1,1 @@
+scp -r * cca:/scratch/ms/nl/nkbs/DALES/KNMI_testbed/
