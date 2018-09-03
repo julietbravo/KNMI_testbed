8ab58e62bcf4685d96e80c11840a6da415b69b78
julietbravo <bart@vanstratum.com>
2018-09-03 09:09:11 +0200

    Update offshore case for interactive radiation

--- cases/offshore/create_input.py
+++ cases/offshore/create_input.py
@@ -8,7 +8,7 @@ import datetime
 from tools import *
 
 # DALES experiment number
-expnr = 0
+expnr = 1
 
 # Location (domain) in NetCDF file
 # 0=FINO1, 1=Goeree, 2=EPL, 3=K13, 4=HKZ, 5=P11B, 6=F3FB1
@@ -32,8 +32,12 @@ nc_data  = xr.open_mfdataset(nc_files)
 t0, t1 = get_start_end_indices(start, end, nc_data.time.values)
 
 # Docstring for DALES input files
-domain    = nc_data.name[0][iloc].values
-docstring = '{}: {} to {}'.format(domain, start, end)
+domain    = nc_data.name[0,iloc].values
+lat       = nc_data.central_lat[0,iloc].values
+lon       = nc_data.central_lon[0,iloc].values
+docstring = '{0} ({1:.2f}N, {2:.2f}E): {3} to {4}'.format(domain, lat, lon, start, end)
+
+print(docstring)
 
 # Create stretched vertical grid for LES
 #grid = Grid_stretched(kmax=160, dz0=20, nloc1=80, nbuf1=20, dz1=130)
@@ -44,7 +48,18 @@ grid = Grid_stretched(kmax=80, dz0=30, nloc1=40, nbuf1=10, dz1=200)    # debug
 create_initial_profiles(nc_data, grid, t0, t1, iloc, docstring, expnr)
 
 # Create and write the surface and atmospheric forcings (ls_flux.inp, ls_fluxsv.inp, lscale.inp)
-create_ls_forcings(nc_data, grid, t0, t1, iloc, docstring, expnr, harmonie_rad=True)
+create_ls_forcings(nc_data, grid, t0, t1, iloc, docstring, expnr, harmonie_rad=False)
 
 # Write the nudging profiles (nudge.inp)
 create_nudging_profiles(nc_data, grid, t0, t1, iloc, docstring, expnr)
+
+# Create NetCDF file with reference profiles for RRTMG
+create_backrad(nc_data, t0, iloc, expnr)
+
+# Update namelist
+namelist = 'namoptions.{0:03d}'.format(expnr)
+replace_namelist_value(namelist, 'xlat',  lat)
+replace_namelist_value(namelist, 'xlon',  lon)
+replace_namelist_value(namelist, 'xday',  start.timetuple().tm_yday)
+replace_namelist_value(namelist, 'xtime', start.hour)
+replace_namelist_value(namelist, 'kmax',  grid.kmax)
