11aaf3dd21f0f742ca04ed2831d4df824caab3f9
julietbravo <bart@vanstratum.com>
2018-12-06 12:17:07 +0000

    Added output of binary files for DALES

--- cases/nudge_boundary_HARMONIE/create_boundaries.py
+++ cases/nudge_boundary_HARMONIE/create_boundaries.py
@@ -39,7 +39,56 @@ class Grid:
         self.yh = np.arange(0, self.ysize, self.dy)
         self.zh = np.arange(0, self.zsize, self.dz)
 
-#def write_LBCs(u, v, thl, qt
+def write_LBC(u, v, thl, qt, itot, jtot, nprocx, procy, t, iexpnr):
+
+    # Size of MPI sub-domains
+    block_x = int(itot / nprocx)
+    block_y = int(jtot / nprocy)
+
+    # Write the zonal boundaries
+    for i in (1, nprocx):
+        for j in range(nprocy):
+
+            # East and west slices
+            if i==1:
+                s = np.s_[:block_x,      j*block_y:(j+1)*block_y, :]
+            else:
+                s = np.s_[itot-block_x:, j*block_y:(j+1)*block_y, :]
+
+            # Open binary file
+            name = 'lbc{0:03d}h{1:02d}m_x{2:03d}y{2:03d}.{3:03d}'.format(t, 0, i, j+1, iexpnr)
+            print('Writing {}'.format(name))
+            f = open(name, 'wb+')
+
+            # Write boundaries and close file
+            u  [s].tofile(f)
+            v  [s].tofile(f)
+            thl[s].tofile(f)
+            qt [s].tofile(f)
+            f.close()
+
+    # Write the meridional boundaries
+    if nprocx > 2:
+        for j in (1, nprocy):
+            for i in range(1,nprocx-1):
+
+                # South and north slices
+                if j==1:
+                    s = np.s_[i*block_x:(i+1)*block_x, :block_y,     :]
+                else:
+                    s = np.s_[i*block_y:(i+1)*block_x, jtot-block_y: :]
+
+                # Open binary file
+                name = 'lbc{0:03d}h{1:02d}m_x{2:03d}y{2:03d}.{3:03d}'.format(t, 0, i+1, j, iexpnr)
+                print('Writing {}'.format(name))
+                f = open(name, 'wb+')
+
+                # Write boundaries and close file
+                u  [s].tofile(f)
+                v  [s].tofile(f)
+                thl[s].tofile(f)
+                qt [s].tofile(f)
+                f.close()
 
 
 
@@ -48,9 +97,11 @@ if __name__ == '__main__':
     pl.close('all')
 
     # Settings
+    iexpnr = 1
+
     # Start and end time (index in HARMONIE files)
     t0 = 0
-    t1 = 3
+    t1 = 1
 
     # Lower left corner LES domain in Harmonie (m)
     x0 = 150000
@@ -67,14 +118,12 @@ if __name__ == '__main__':
     ktot = 128
 
     # Number of x,y MPI processes
-    nprocx = 2
-    nprocy = 2
-
-    # Width of the lateral boundaries (# gridpoints)
-    n_lbc = 10
+    nprocx = 4
+    nprocy = 4
 
     # DALES constants (modglobal.f90)
-    cd = dict(p0=1.e5, Rd=287.04, cp=1004., Lv=2.53e6)
+    cd = dict(p0=1.e5, Rd=287.04, Rv=461.5, cp=1004., Lv=2.53e6)
+    cd['eps'] = cd['Rv']/cd['Rd']-1.
 
     # LES grid
     grid = Grid(xsize, ysize, zsize, itot, jtot, ktot)
@@ -83,12 +132,13 @@ if __name__ == '__main__':
     grid_sig = hsg.Sigma_grid('data/H40_65lev.txt')
 
     # HARMONIE data
-    u  = xr.open_dataset('/Users/bart/meteo/data/DOWA_fulldomain/2010/02/28/00/ua.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc')
-    v  = xr.open_dataset('/Users/bart/meteo/data/DOWA_fulldomain/2010/02/28/00/va.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc')
-    T  = xr.open_dataset('/Users/bart/meteo/data/DOWA_fulldomain/2010/02/28/00/ta.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc')
-    q  = xr.open_dataset('/Users/bart/meteo/data/DOWA_fulldomain/2010/02/28/00/hus.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc')
-    ql = xr.open_dataset('/Users/bart/meteo/data/DOWA_fulldomain/2010/02/28/00/clw.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc')
-    ps = xr.open_dataset('/Users/bart/meteo/data/DOWA_fulldomain/2010/02/28/00/ps.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc')
+    data_path = '/nobackup/users/stratum/DOWA/DOWA_fulldomain/2010/02/28/00'
+    u  = xr.open_dataset('{}/ua.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc'.format(data_path))
+    v  = xr.open_dataset('{}/va.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc'.format(data_path))
+    T  = xr.open_dataset('{}/ta.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc'.format(data_path))
+    q  = xr.open_dataset('{}/hus.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc'.format(data_path))
+    ql = xr.open_dataset('{}/clw.Slev.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc'.format(data_path))
+    ps = xr.open_dataset('{}/ps.his.NETHERLANDS.DOWA_40h12tg2_fERA5_ptA.20100228.nc'.format(data_path))
 
     # Select a sub-area around the LES domain, to speed up
     # calculations done over the entire HARMONIE grid
@@ -112,9 +162,12 @@ if __name__ == '__main__':
         u_t  = u [t,:,:,:].values
         v_t  = v [t,:,:,:].values
         T_t  = T [t,:,:,:].values
-        q_t  = q [t,:,:,:].values
+        qv_t = q [t,:,:,:].values
         ql_t = ql[t,:,:,:].values
         ps_t = ps[t,:,:  ].values
+
+        # Virtual temperature for height calculation
+        Tv_t = T_t * (1+cd['eps']*qv_t - ql_t)
 
         # Calculate pressure and height on full and half HARMONIE grid levels
         ph = grid_sig.calc_half_level_pressure(ps_t)
@@ -126,7 +179,7 @@ if __name__ == '__main__':
         exner = (p/cd['p0'])**(cd['Rd']/cd['cp'])
         th_t  = T_t / exner
         thl_t = th_t - cd['Lv'] / (cd['cp'] * exner) * ql_t
-        qt_t  = q_t + ql_t
+        qt_t  = qv_t + ql_t
 
         # Create the interpolator for HARMONIE -> LES
         intp = ip.Grid_interpolator(u['x'].values, u['y'].values, z, grid.x, grid.y, grid.z, grid.xh, grid.yh, grid.zh, x0, y0)
@@ -134,20 +187,13 @@ if __name__ == '__main__':
         # Interpolate HARMONIE onto LES grid
         # `::-1` reverses the vertical dimension (HARMONIE's data
         # is aranged from top-to-bottom, LES from bottom-to-top
-        u_LES   = intp.interpolate(u_t  [::-1,:,:], 'xh', 'y', 'z')
-        v_LES   = intp.interpolate(v_t  [::-1,:,:], 'x', 'yh', 'z')
-        thl_LES = intp.interpolate(thl_t[::-1,:,:], 'x', 'y',  'z')
-        ql_LES  = intp.interpolate(qt_t [::-1,:,:], 'x', 'y',  'z')
+        u_LES   = intp.interpolate(u_t  [::-1,:,:], 'xh', 'y',  'z')
+        v_LES   = intp.interpolate(v_t  [::-1,:,:], 'x',  'yh', 'z')
+        thl_LES = intp.interpolate(thl_t[::-1,:,:], 'x',  'y',  'z')
+        ql_LES  = intp.interpolate(qt_t [::-1,:,:], 'x',  'y',  'z')
 
-
-#        # Write lateral boundaries as binary files which can directly be read by DALES
-#        # 1. Zonal boundaries
-#        #block_x = int(itot / nprocx)
-#        #block_y = int(jtot / nprocy)
-#        #for j in range(nprocy):
-#        #    # 1.1 Left (west) boundary
-#        #    sy = np.s_[0:n_lbc, j*block_y:(j+1)*block_y, :]
-#
+        # Write the LBCs in binary format for LES
+        write_LBC(u_LES, v_LES, thl_LES, ql_LES, itot, jtot, nprocx, nprocy, t, iexpnr)
 
 
 
