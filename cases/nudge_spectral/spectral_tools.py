0a752aab6c2b69f007d0333781bea5d00d90ccd6
julietbravo <bart@vanstratum.com>
2019-09-30 12:14:18 +0200

    Added radialy averaged spectra

--- cases/nudge_spectral/spectral_tools.py
+++ cases/nudge_spectral/spectral_tools.py
@@ -7,41 +7,37 @@ pl.ion()
 
 np.random.seed(3)
 
-def radial_distance(itot, jtot):
+def calc_radial_distance(itot, jtot):
 
     j0 = jtot//2
     jj,ii = np.indices((jtot, itot//2+1))
-    jj -= j0    # Shift center
+    jj -= j0
     return np.sqrt(ii**2 + jj**2)
 
 
-def spectral_mask(itot, jtot, k0):
+def calc_spectral_mask(itot, jtot, k0):
 
-    dist = radial_distance(itot, jtot)
+    dist = calc_radial_distance(itot, jtot)
     mask = dist <= k0
 
     return mask.astype(int)
 
 
-def spectral_filt_2d(field, k0, method='low'):
+def spectral_filter_2d(field, k0, method='low'):
 
     jtot = field.shape[0]
     itot = field.shape[1]
 
     # 2D forward FFT
     f = np.fft.rfft2(field)
-
-    # Result of 2D FFT = real fft over last dimension, complex over 
-    # first dimension. Shift the complex part for convenience
     f = np.fft.fftshift(f, axes=0)
 
-    # Create/define wavenumber mask
-    mask = spectral_mask(itot, jtot, k0)
+    # Create/define/apply wavenumber mask
+    mask = calc_spectral_mask(itot, jtot, k0)
     
     if method == 'high':
         mask = 1.-mask
 
-    # Apply mask
     f *= mask
 
     # Shift back, and calculate/return 2D backward FFT
@@ -57,13 +53,11 @@ def spectral_blend_2d(field1, field2, k0):
     f1 = np.fft.rfft2(field1)
     f2 = np.fft.rfft2(field2)
 
-    # Result of 2D FFT = real fft over last dimension, complex over 
-    # first dimension. Shift the complex part for convenience
     f1 = np.fft.fftshift(f1, axes=0)
     f2 = np.fft.fftshift(f2, axes=0)
 
     # Create/define wavenumber mask
-    mask1 = spectral_mask(itot, jtot, k0)
+    mask1 = calc_spectral_mask(itot, jtot, k0)
     mask2 = 1-mask1
 
     # Blend
@@ -76,25 +70,22 @@ def spectral_blend_2d(field1, field2, k0):
 def spectral_noise(itot, jtot, k, fac, ks, ampl):
 
     # Generate 2D spectrum with random noise
-    arnd = 2*np.pi*np.random.rand(jtot, itot//2+1)
-    afftrnd = np.cos(arnd) + 1j*np.sin(arnd)
+    rand = 2*np.pi*np.random.rand(jtot, itot//2+1)
+    fft_rand = np.cos(rand) + 1j*np.sin(rand)
 
     # Shift small wavenumbers to center
-    afftrnd = np.fft.fftshift(afftrnd, axes=0)
+    fft_rand = np.fft.fftshift(fft_rand, axes=0)
 
     # Calculate the radial wave numbers
-    l = radial_distance(itot, jtot)
+    l = calc_radial_distance(itot, jtot)
 
     # Filter on radial wave number using a Gaussian function
-    if isinstance(k, list):
-        factor = np.zeros_like(arnd)
-        for i in range(len(k)):
-            factor += fac[i]*np.exp(-(l-k[i])**2. / (2.*ks[i]))
-    else:
-        factor  = fac*np.exp(-(l-k)**2. / (2.*ks))
+    factor = np.zeros_like(rand)
+    for i in range(len(k)):
+        factor += fac[i]*np.exp(-(l-k[i])**2. / (2.*ks[i]))
 
     # Create the filtered field
-    afft = factor*afftrnd
+    afft = factor*fft_rand
 
     # Inverse shift & 2D FFT
     a = np.fft.irfft2(np.fft.ifftshift(afft, axes=0))
@@ -105,7 +96,32 @@ def spectral_noise(itot, jtot, k, fac, ks, ampl):
     return a
 
 
+def radial_spectrum(field, kmax=None):
 
+    jtot = field.shape[0]
+    itot = field.shape[1]
+
+    # Calculate 2D FFT, remove mean, and shift output
+    fft = np.fft.rfft2(field) / (itot*jtot)
+    fft[0,0] = 0.
+    fft = np.fft.fftshift(fft, axes=0)
+
+    # Radial distance
+    l = calc_radial_distance(itot, jtot)
+
+    # Calculate spectral power
+    power = fft.real**2 + fft.imag**2
+    power[:,1:-1] *= 2
+
+    # Bin on wavenumber
+    if kmax is None:
+        kmax = int(np.sqrt((itot//2+1)**2 + (jtot//2+1)**2))
+    bin_edge = np.arange(0, kmax+1, 1)
+
+    n, bin_edges = np.histogram(l.flatten(), bin_edge, weights=power.flatten())
+    bins = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+
+    return bins, n
 
 
 if True:
@@ -126,32 +142,75 @@ if True:
     ky = np.arange(jtot/2+1)
 
 
-
-
-
     if True:
-        z1 = spectral_noise(itot, jtot, k=2, fac=1, ks=1, ampl=2)
-        z2 = spectral_noise(itot, jtot, k=[4,20], fac=[2,1], ks=[3,1], ampl=1)
-        z3 = spectral_blend_2d(z1, z2, 5)
+        z1 = spectral_noise(itot, jtot, k=[2],  fac=[1], ks=[3], ampl=3)
+        z2 = spectral_noise(itot, jtot, k=[20], fac=[1], ks=[10], ampl=1)
+        z3 = spectral_blend_2d(z1, z2, 10)
 
         if True:
-            pl.figure(figsize=(10,4))
+            f = 200 / xsize
+
+
+            pl.figure(figsize=(11,3.8))
             pl.subplot(131, aspect='equal')
-            pl.pcolormesh(x, y, z1, cmap=pl.cm.RdBu_r)
+            pl.title('Large scale', loc='left')
+            pl.pcolormesh(f*x, f*y, z1, cmap=pl.cm.RdBu_r)
+            pl.xlabel(r'$x$ (km)')
+            pl.ylabel(r'$y$ (km)')
 
             pl.subplot(132, aspect='equal')
-            pl.pcolormesh(x, y, z2, cmap=pl.cm.RdBu_r)
+            pl.title('Small scale', loc='left')
+            pl.pcolormesh(f*x, f*y, z2, cmap=pl.cm.RdBu_r)
+            pl.xlabel(r'$x$ (km)')
+            pl.ylabel(r'$y$ (km)')
 
             pl.subplot(133, aspect='equal')
-            pl.pcolormesh(x, y, z3, cmap=pl.cm.RdBu_r)
+            pl.title('Blended', loc='left')
+            pl.pcolormesh(f*x, f*y, z3, cmap=pl.cm.RdBu_r)
+            pl.xlabel(r'$x$ (km)')
+            pl.ylabel(r'$y$ (km)')
 
             pl.tight_layout()
 
             pl.figure()
             pl.plot(x, z1[jtot//2,:], 'k-', label='Large scale')
-            pl.plot(x, z2[jtot//2,:], 'r-', label='Small scale')
-            pl.plot(x, z3[jtot//2,:], 'k--', label='Blended')
+            pl.plot(x, z2[jtot//2,:], 'k-', label='Small scale', dashes=[5,2])
+            pl.plot(x, z3[jtot//2,:], 'r-', label='Blended')
+            pl.legend()
 
+
+
+        b1, f1 = radial_spectrum(z1, kmax=30)
+        b2, f2 = radial_spectrum(z2, kmax=30)
+        b3, f3 = radial_spectrum(z3, kmax=30)
+
+
+        pl.figure()
+        pl.semilogy(b1, f1, 'k--x', label='large scale')
+        pl.semilogy(b2, f2, 'k--o', label='small scale')
+        pl.semilogy(b3, f3, 'r-', linewidth=1.5, label='blended')
+        pl.legend()
+        pl.xlabel('K')
+        pl.ylabel('power')
+
+
+
+
+#for n in range(time.size):
+#    qtpath = data.variables['qtpath'][n,:,:]
+#    qtpath_fft = np.fft.rfft2(qtpath) / (x.size*y.size)
+#    qtpath_fft[0,0] = 0.
+#    qtpath_fft_power = abs(qtpath_fft)**2
+#    qtpath_fft_power[:,1:-1] *= 2
+#    qtpath_spectra[n,:,:] = qtpath_fft_power
+#​
+#bin_seq = (2.*np.pi/L)*np.arange(.5, k.size+10, 2)
+#n, bin_edges = np.histogram(K.flatten(), bin_seq, weights=(qtpath_spectra[0,:,:]).flatten())
+#pdfy = np.zeros((time.size, n.size))
+#pdfx = 0.5*(bin_edges[0:-1] + bin_edges[1:])
+#for i in range(time.size):
+#    n, bins = np.histogram(K.flatten(), bin_seq, weights=(qtpath_spectra[i,:,:]).flatten())
+#    pdfy[i,:] = n[:]
 
 
 
@@ -165,8 +224,8 @@ if True:
             z[j,:] += np.cos(x*5)
             z[j,:] += np.cos(x*10)
 
-        zz1, mask1 = spectral_filt_2d(z, 5, 'low')
-        zz2, mask2 = spectral_filt_2d(z, 5, 'high')
+        zz1, mask1 = spectral_filter_2d(z, 5, 'low')
+        zz2, mask2 = spectral_filter_2d(z, 5, 'high')
 
         pl.figure()
         pl.subplot(231)
